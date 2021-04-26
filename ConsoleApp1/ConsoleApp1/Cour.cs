cb1ea63b0d1a51220d81da59ccc4b8081842b489
GabtheGreen <77289547+GabtheGreen@users.noreply.github.com>
2021-04-25 21:45:59 -0400

    second version

---
+++ ConsoleApp1/ConsoleApp1/Cour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Cour
+    {
+        private static int numeroCoursDeBase = 1000000;
+        private string titreDeCour { get; set; }
+        private string codeDeCour { get; set; }
+        private int numerosDeCour { get; set; }
+
+        public Cour(string titre, string code)
+        {
+            this.numerosDeCour = ++numeroCoursDeBase;
+            this.titreDeCour = titre;
+            this.codeDeCour = code;
+        }
+
+    }
+}
